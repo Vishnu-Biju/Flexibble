b1b11c438d4b1ec4b17fb33f6d7048065a7ef32d
vishnu biju <vishnubiju.in@gmail.com>
2023-07-10 17:19:24 +0530

    update: Navbar updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,46 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { NavLinks } from "@/constants";
 
-const Navbar = () => {
+
+import AuthProviders from "./AuthProviders";
+
+const Navbar = async () => {
+  const session = {};
+
   return (
-    <nav className="flexBetween navbar">
-      <div className="flex-1 flexStart gap-10">
-        <Link href="/">
+    <nav className='flexBetween navbar'>
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
           <Image
-            src="/logo.svg"
-            width={115}
-            height={43} 
-            alt="Flexibble" 
-            />
+            src='/logo.svg'
+            width={116}
+            height={43}
+            alt='logo'
+          />
         </Link>
-        <ul className="xl:flex hidden text-small gap-7">
-
+        <ul className='xl:flex hidden text-small gap-7'>
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.text}>
+              {link.text}
+            </Link>
+          ))}
         </ul>
+      </div>
+
+      <div className='flexCenter gap-4'>
+        {session? (
+          <>
+           
+
+            <Link href="/create-project">
+              
+            </Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
       </div>
     </nav>
   );
