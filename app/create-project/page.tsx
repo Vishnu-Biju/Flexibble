79da7ef1c8e3c83cac337b4d74e26ea6aa412c60
vishnu biju <vishnubiju.in@gmail.com>
2023-07-12 14:20:56 +0530

    feat: share project page created

---
+++ app/create-project/page.tsx
@@ -0,0 +1,20 @@
+
+
+
+import { redirect } from "next/navigation";
+import { getCurrentUser } from "@/lib/session";
+
+import Modal from "@/components/Modal";
+import ProjectForm from "@/components/ProjectForm";
+
+const CreateProject = () => {
+  return (
+    <Modal>
+      <h3 className="modal-head-text">Create a New Project</h3>
+
+      <ProjectForm  />
+    </Modal>
+  )
+}
+
+export default CreateProject
