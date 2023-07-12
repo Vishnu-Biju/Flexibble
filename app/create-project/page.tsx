dacb7a041a557d90bd6e91c989f2762e8dc2590c
vishnu biju <vishnubiju.in@gmail.com>
2023-07-12 17:12:49 +0530

    update: page-button

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -1,20 +1,21 @@
-
-
-
 import { redirect } from "next/navigation";
-import { getCurrentUser } from "@/lib/session";
 
+import { getCurrentUser } from "@/lib/session";
 import Modal from "@/components/Modal";
 import ProjectForm from "@/components/ProjectForm";
 
-const CreateProject = () => {
+const CreateProject = async () => {
+  const session = await getCurrentUser();
+
+  if (!session?.user) redirect("/")
+
   return (
     <Modal>
       <h3 className="modal-head-text">Create a New Project</h3>
 
-      <ProjectForm  />
+      <ProjectForm type="create" session={session} />
     </Modal>
-  )
-}
+  );
+};
 
-export default CreateProject
+export default CreateProject;
