769d48f4169477bf1927e798c578ed3a686290e4
vishnu biju <vishnubiju.in@gmail.com>
2023-07-13 12:36:16 +0530

    update: layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
-import Navbar from "@/components/Navbar";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "Flexibble",
-  description: "Show case and discover remarkable developer projects",
+  description: "Showcase and discover remarkable developer projects",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         <Navbar />
-        <main>{children}</main>
+        <main>
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
